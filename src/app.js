fae66277cad35b2c35530278fe521ae4e86a005b
Prithvi <prithvirajkumar14@gmail.com>
2020-12-31 19:50:49 -0500

    Inital commit

--- src/app.js
+++ src/app.js
@@ -18,18 +18,6 @@ import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
 
-store.subscribe(() => {
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-  console.log(visibleExpenses);
-});
-
-store.dispatch(addExpense({ description: "Water Bill", amount: 4500 }));
-store.dispatch(addExpense({ description: "Gas Bill", amount: 300 }));
-store.dispatch(
-  addExpense({ description: "Rent", amount: 109500, createdAt: 1000 })
-);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter></AppRouter>
