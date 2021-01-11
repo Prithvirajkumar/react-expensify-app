162a772e64837c77fcfb49d26bbcd77b16f05de9
Prithvi <prithvirajkumar14@gmail.com>
2021-01-10 21:17:28 -0500

    Added Expensify styles

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,16 +5,25 @@ import selectExpenses from "../selectors/expenses";
 
 export const ExpenseList = (props) => {
   return (
-    <div>
-      {props.expenses.length === 0 ? (
-        <p>No Expenses</p>
-      ) : (
-        props.expenses.map((expense) => {
-          return (
-            <ExpenseListItem key={expense.id} {...expense}></ExpenseListItem>
-          );
-        })
-      )}
+    <div className="content-container">
+      <div className="list-header">
+        <div className="show-for-mobile">Expenses</div>
+        <div className="show-for-desktop">Expense</div>
+        <div className="show-for-desktop">Amount</div>
+      </div>
+      <div className="list-body">
+        {props.expenses.length === 0 ? (
+          <div className="list-item list-item--message">
+            <span>No Expenses</span>
+          </div>
+        ) : (
+          props.expenses.map((expense) => {
+            return (
+              <ExpenseListItem key={expense.id} {...expense}></ExpenseListItem>
+            );
+          })
+        )}
+      </div>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { startLogout } from "../actions/auth";
 
 export const Header = ({ startLogout }) => {
   return (
-    <header>
-      <h1>Expensify</h1>
-      <NavLink to="/dashboard" activeClassName="is-active" exact={true}>
-        Dashboard
-      </NavLink>
-      <NavLink to="/create" activeClassName="is-active" exact={true}>
-        Create Expense
-      </NavLink>
-      <button onClick={startLogout}>Logout</button>
+    <header className="header">
+      <div className="content-container">
+        <div className="header__content">
+          <Link className="header__title" to="/dashboard" exact={true}>
+            <h1>Expensify</h1>
+          </Link>
+          <button className="button button--link" onClick={startLogout}>
+            Logout
+          </button>
+        </div>
+      </div>
     </header>
   );
 };
